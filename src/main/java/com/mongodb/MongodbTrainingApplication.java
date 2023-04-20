80218ab7b949d75569e2d8bd48c6542db746000f
Romulo Matheus <romulomatheus31@gmail.com>
2023-04-20 11:29:53 -0300

    first commit

---
+++ src/main/java/com/mongodb/MongodbTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbTrainingApplication.class, args);
+	}
+
+}
